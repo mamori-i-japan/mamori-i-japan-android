fad6d7ca035cf868911733f4bbfef8d76dbcce28
Kenta Nakahira <knakahir1989@gmail.com>
2020-04-21 13:35:07 +0900

    陽性者リストのtempIDのキー名変更

--- app/src/main/java/jp/co/tracecovid19/data/model/PositivePerson.kt
+++ app/src/main/java/jp/co/tracecovid19/data/model/PositivePerson.kt
@@ -2,4 +2,4 @@ package jp.co.tracecovid19.data.model
 
 import com.squareup.moshi.Json
 
-data class PositivePerson(@Json(name = "uuid") val tempId: String)
+data class PositivePerson(@Json(name = "tempID") val tempId: String)
