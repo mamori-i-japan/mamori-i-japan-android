6c0dc8049850ee59354c523c632cd36631d92f60
Kenta Nakahira <knakahir1989@gmail.com>
2020-04-24 12:00:11 +0900

    timeの変換メソッド名を修正

--- app/src/main/java/jp/co/tracecovid19/data/model/DeepContact.kt
+++ app/src/main/java/jp/co/tracecovid19/data/model/DeepContact.kt
@@ -2,25 +2,24 @@ package jp.co.tracecovid19.data.model
 
 import com.squareup.moshi.Json
 import jp.co.tracecovid19.data.database.deepcontactuser.DeepContactUserEntity
-import jp.co.tracecovid19.extension.convertSHA256HashString
-import jp.co.tracecovid19.extension.convertTimeInMillis
-import jp.co.tracecovid19.extension.convertUnixTime
+import jp.co.tracecovid19.extension.*
 
 data class DeepContact(val uniqueInsertKey: String,
                        @Json(name = "externalTempId") val tempId: String,
                        private val contactStartTime: Long,
                        private val contactEndTime: Long) {
 
-    val startTime = contactStartTime.convertTimeInMillis()
-    val endTime = contactEndTime.convertTimeInMillis()
+    val startTime = contactStartTime.convertToTimeInMillis()
+    val endTime = contactEndTime.convertToTimeInMillis()
 
     companion object {
         fun create(entity: DeepContactUserEntity): DeepContact {
+            // BE側はUnixTimeなのでTimeInMillisとの相互変換が必要
             return DeepContact(
                 (entity.tempId + entity.startTime.toString() + entity.endTime.toString()).convertSHA256HashString(),
                 entity.tempId,
-                entity.startTime.convertUnixTime(),
-                entity.endTime.convertUnixTime())
+                entity.startTime.convertToUnixTime(),
+                entity.endTime.convertToUnixTime())
         }
     }
 }
--- app/src/main/java/jp/co/tracecovid19/data/model/TempUserId.kt
+++ app/src/main/java/jp/co/tracecovid19/data/model/TempUserId.kt
@@ -2,23 +2,23 @@ package jp.co.tracecovid19.data.model
 
 import com.squareup.moshi.Json
 import jp.co.tracecovid19.data.database.tempuserid.TempUserIdEntity
-import jp.co.tracecovid19.extension.convertTimeInMillis
-import jp.co.tracecovid19.extension.convertUnixTime
+import jp.co.tracecovid19.extension.convertToTimeInMillis
+import jp.co.tracecovid19.extension.convertToUnixTime
 
 data class TempUserId(@Json(name = "tempID") val tempId: String,
                       private val validFrom: Long,
                       private val validTo: Long) {
 
-    val startTime = validFrom.convertTimeInMillis()
-    val expiryTime = validTo.convertTimeInMillis()
+    val startTime = validFrom.convertToTimeInMillis()
+    val expiryTime = validTo.convertToTimeInMillis()
 
     companion object {
 
         fun create(entity: TempUserIdEntity): TempUserId {
             // BE側はUnixTimeなのでTimeInMillisとの相互変換が必要
             return TempUserId(entity.tempId,
-                entity.startTime.convertUnixTime(),
-                entity.expiryTime.convertUnixTime())
+                entity.startTime.convertToUnixTime(),
+                entity.expiryTime.convertToUnixTime())
         }
     }
 }
