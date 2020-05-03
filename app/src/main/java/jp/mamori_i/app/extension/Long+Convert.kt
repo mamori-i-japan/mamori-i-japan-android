75b1231f030d4b320a96fff87c63c925176a922d
t-matsukuma <matsukuma74@gmail.com>
2020-05-04 01:55:20 +0900

    昨日の濃厚接触情報取得、TempIdの直近2週間、2週間以前のデータ削除を実装

--- app/src/main/java/jp/mamori_i/app/extension/Long+Convert.kt
+++ app/src/main/java/jp/mamori_i/app/extension/Long+Convert.kt
@@ -17,3 +17,15 @@ fun Long.convertToTimeInMillis(): Long {
 fun Long.convertToUnixTime(): Long {
     return this / 1000
 }
+
+fun Long.yesterdayPeriod(): Pair<Long, Long> {
+    val ymd = this.convertToDateTimeString("yyyyMMdd")
+    val to = (ymd + "000000").convertToUnixTime("yyyyMMddHHmmss")
+    val from = (ymd + "000000").convertToUnixTime("yyyyMMddHHmmss") - 24 * 60 * 60 * 1000
+    return from to to
+}
+
+fun Long.twoWeeks(): Long {
+    val ymd = this.convertToDateTimeString("yyyyMMdd")
+    return (ymd + "000000").convertToUnixTime("yyyyMMddHHmmss") - 14 * 24 * 60 * 60 * 1000
+}
